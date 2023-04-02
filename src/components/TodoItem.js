166a5708788e4f288f1baf413b531c8164374cec
Hyejin Kang <kkamboting@gmail.com>
2023-04-02 17:52:15 +0900

    0402_addlist_상태변경

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { TextField } from '@mui/material';
+import Button from '@mui/material/Button';
 
 const TodoItem = (probs) => {
 
+    const style = probs.item.isFinished ? {textDecoration: 'line-through'} : {};
+
     return(
 
-        <div className="todo-item">
-            {probs.item}
+        <div className="todo-item" style={style} onClick={() => probs.completeTodo(probs.item)}>
+            {probs.item.item}
         </div>
 
     );
