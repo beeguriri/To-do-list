501cc175d0e4acf75c6f6304f4c4ec9edc47c0b2
Hyejin Kang <122770696+beeguriri@users.noreply.github.com>
2023-04-02 16:12:24 +0900

    addList 구조만들기 (#1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck, faPen, faTrashCan } from '@fortawesome/free-regular-svg-icons';
+
 import './App.css';
+import TodoBoard from './components/TodoBoard';
 
 function App() {
+
+  const[inputValue, setInputValue] = useState('');
+  const[todoList, setTodoList] = useState([]);
+  
+  const addItem = () => {
+
+    console.log('addItem :', inputValue)
+    //기존아이템은 유지하고, 새로운 inputvalue 추가
+    setTodoList([...todoList, inputValue])
+  }
+
+
   return (
-    <div className="App">
-      abcd
+    <div className="container App">
+      <br /><br />
+      <input value={inputValue} type="text" onChange=
+        {(event) => setInputValue(event.target.value)} />
+      <button onClick={addItem}>추가</button>
+      <br /><br />
+      <TodoBoard todoList = {todoList} />
+      <br /><br />
+
     </div>
   );
 }
