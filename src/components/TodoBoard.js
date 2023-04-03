94b687f2eed7ff8717ec7d405ea3cf8009562720
Hyejin Kang <122770696+beeguriri@users.noreply.github.com>
2023-04-03 21:49:19 +0900

    Update 구현 (#5)
    * 0402_update기능구현중
    
    * 0403_update기능 구현

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -8,10 +8,14 @@ const TodoBoard = (probs) => {
 
         <div>
             <h4>List</h4>
-            {probs.todoList.map((item)=>
-                <TodoItem key={item.id++} item={item} 
-                    completeTodo={probs.completeTodo} 
-                    deleteTodo={probs.deleteTodo} />)}
+            {probs.todoList
+                //id 내림차순 정렬 (최근에 입력된 것이 제일 위에 올수있도록!)
+                .sort((a,b) => a.id > b.id ? -1 : 1)
+                .map((item)=>
+                    <TodoItem key={item.id} item={item} 
+                        completeTodo={probs.completeTodo} 
+                        deleteTodo={probs.deleteTodo}
+                        setUpdateItem={probs.setUpdateItem} />)}
         </div>
 
     )
