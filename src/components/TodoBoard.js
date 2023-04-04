935b52c4d02f35a57dc77520f3fce313d273cbae
Hyejin Kang <kkamboting@gmail.com>
2023-04-04 23:40:54 +0900

    0404_css수정

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -6,8 +6,7 @@ const TodoBoard = (probs) => {
     console.log(probs)
     return(
 
-        <div>
-            <h4>List</h4>
+        <>
             {probs.todoList
                 //id 내림차순 정렬 (최근에 입력된 것이 제일 위에 올수있도록!)
                 .sort((a,b) => a.id > b.id ? -1 : 1)
@@ -16,7 +15,7 @@ const TodoBoard = (probs) => {
                         completeTodo={probs.completeTodo} 
                         deleteTodo={probs.deleteTodo}
                         setUpdateItem={probs.setUpdateItem} />)}
-        </div>
+        </>
 
     )
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,9 @@
 import React from "react";
-import Button from '@mui/material/Button';
+import ListItem from '@mui/material/ListItem';
+import IconButton from '@mui/material/IconButton';
+import BorderColorRoundedIcon from '@mui/icons-material/BorderColorRounded';
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import { pink } from '@mui/material/colors';
 
 const TodoItem = (probs) => {
 
@@ -7,17 +11,38 @@ const TodoItem = (probs) => {
 
     return(
         <div className="todo-item">
-            <div className="item" style={style} onClick={() => probs.completeTodo(probs.item)}>
-                {probs.item.item}
+            <div className="item">
+                <ul>
+                    <li className="item" style={style} onClick={() => probs.completeTodo(probs.item)}>
+                        {probs.item.item}
+                    </li>
+                </ul>
             </div>
-            <Button variant="outlined" onClick={() => probs.setUpdateItem({
-                    
-                    // Edit버튼 누르면 item제목이 inputbox에 출력되도록 세팅
-                    id: probs.item.id,
-                    item: probs.item.item,
-                    isFinished: probs.item.isFinished
-            })}>Edit</Button>
-            <Button variant="outlined" onClick={() => probs.deleteTodo(probs.item)}>Remove</Button>
+
+            <div className="buttonarea">
+
+                <ListItem secondaryAction={
+                    <IconButton edge="end" aria-label="comments" onClick={() => probs.setUpdateItem({
+                        
+                        // Edit버튼 누르면 item제목이 inputbox에 출력되도록 세팅
+                        id: probs.item.id,
+                        item: probs.item.item,
+                        isFinished: probs.item.isFinished
+                })}>
+                        <BorderColorRoundedIcon sx={{ color: pink[500] }} />
+                    </IconButton>
+                }> </ListItem>
+                
+
+                {/* <Button variant="outlined" onClick={() => probs.deleteTodo(probs.item)}>Remove</Button> */}
+
+                <ListItem secondaryAction={
+                    <IconButton edge="end" aria-label="comments" onClick={() => probs.deleteTodo(probs.item)}>
+                        <DeleteRoundedIcon sx={{ color: pink[500] }} />
+                    </IconButton>
+                }> </ListItem>
+
+            </div>
         </div>
     );
 }
